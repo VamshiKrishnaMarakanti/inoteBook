99283d14ad39a0b72c05d2033ef5734ac0aa597d
vamshi Krishna Marakanti <vamshimarakanti@gmail.com>
2025-06-21 06:48:53 +0530

    MakeRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter
+} from "react-router-dom";
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <BrowserRouter>
+    <Navbar/>
+    <h1> This is iNoteBook</h1>
+    <Routes>
+      
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/About" element={<About/>}></Route>       
+       </Routes> 
+       </BrowserRouter>
+  </>
   );
 }
 
